6b7b0ed9561b6b8edfa2a8230a4708835ed10079
student6 <student6@NIBMADMIN12s-iMac.local>
2026-06-06 16:48:24 +0530

    first commit

---
+++ ios-project/ios-project/ios_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_projectApp.swift
+//  ios-project
+//
+//  Created by student6 on 2026-06-06.
+//
+
+import SwiftUI
+
+@main
+struct ios_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
