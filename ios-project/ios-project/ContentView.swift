0e31496103174adcd539c9e2510a9cec73de5dc8
Pubudu <hasith819@gmail.com>
2026-06-13 12:51:21 +0530

    Create UI

--- ios-project/ios-project/ContentView.swift
+++ ios-project/ios-project/ContentView.swift
@@ -10,11 +10,32 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Text("Score:0")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.top, 50)
+            
+            Spacer()
+            
+            Button("Tap") {
+                
+            }
+            .font(Font.largeTitle)
+            .fontWeight(.bold)
+            .frame(width:200, height:200)
+            .foregroundStyle(.white)
+            .background(.blue)
+            .clipShape(Circle())
+
+            Spacer()
+            
+            Text("Time:10")
+                .font(.title)
+                .fontWeight(.semibold)
+                .padding(.top, 20)
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .padding()
     }
 }
